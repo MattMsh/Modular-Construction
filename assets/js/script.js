62fdbf1d570525c21688b63c4cfda4a0bbac1ca2
Matthew4Jesus <matvii.mashtalyar@gmail.com>
2020-01-28 12:48:18 +0200

    added styles and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,25 @@ $(".menu-burger").on("click", function(e) {
     $(this).toggleClass("active");
     $("body").toggleClass("menu-open");
 });
+
+$("input.search-input").focus(function() {
+    $(".clear-input-wrap").addClass("focus");
+});
+   
+
+$("input.search-input").blur(function() {
+    if($(this).val() != ""){
+        $("label[for='dynamic-label-input']").addClass("focus");
+    }
+    else {
+        $("label[for='dynamic-label-input']").removeClass("focus");
+        $(".clear-input-wrap").removeClass("focus");
+    }
+    
+});
+
+$(".clear-input-wrap").on("click", function(e) {
+    e.preventDefault();
+    $("input.search-input").val("");
+    $("input.search-input").blur();
+});
