3dfcbede20fe4981cec1e9b90b31620a6c23cbd3
Matthew4Jesus <matvii.mashtalyar@gmail.com>
2020-01-29 15:20:37 +0200

    added script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,69 @@
-$(".menu-burger").on("click", function(e) {
+jQuery(document).ready(function($){
+  // Burger menu
+  $(".menu-burger").on("click", function(e) {
     e.preventDefault();
     $(this).toggleClass("active");
     $("body").toggleClass("menu-open");
-});
+  });
 
-$("input.search-input").focus(function() {
+  // Input styles
+  $("input.search-input").focus(function() {
     $(".clear-input-wrap").addClass("focus");
-});
-   
-
-$("input.search-input").blur(function() {
+  });
+     
+  $("input.search-input").blur(function() {
     if($(this).val() != ""){
-        $("label[for='dynamic-label-input']").addClass("focus");
+      $("label[for='dynamic-label-input']").addClass("focus");
     }
     else {
-        $("label[for='dynamic-label-input']").removeClass("focus");
-        $(".clear-input-wrap").removeClass("focus");
-    }
-    
-});
+      $("label[for='dynamic-label-input']").removeClass("focus");
+      $(".clear-input-wrap").removeClass("focus");
+    }   
+  });
 
-$(".clear-input-wrap").on("click", function(e) {
+  $(".tell-us-input").focus(function() {
+    $(this).parent().addClass("focus");
+  });
+
+  $(".tell-us-input").blur(function() {
+    if($(this).val() != "") {
+      $(this).parent().addClass("focus");
+    }
+    else {
+      $(this).parent().removeClass("focus");
+    }
+  });  
+
+  $(".clear-input-wrap").on("click", function(e) {
     e.preventDefault();
     $("input.search-input").val("");
     $("input.search-input").blur();
+  });
+
+  // Filter
+  $(".filter").on("click", function() {
+    $(this).toggleClass("open");
+    $(".filter ul").slideToggle();
+  });
+
+  $(document).on("click", '.filter .filter-item', function() {
+    $(".filter-item-active-text").text($(this).text());
+  });
+
+  $(".filter-item").on("click", function() {
+    $(".filter-item").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $(".projects-filter-item").on("click", function() {
+    $(".projects-filter-item").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  // Slick
+  
+  $('.other-project-slider').slick();
 });
+
+
+
